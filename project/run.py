9b3418efb6b11707c12fbb54c2f8156a1e1b8764
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-04-04 18:20:23 +0400

    run multithreading added

--- project/run.py
+++ project/run.py
@@ -1,4 +1,5 @@
 import datetime
+import threading
 
 from parsers.barona import Barona
 from parsers.eezy import Eezy
@@ -15,14 +16,37 @@ import random
 db = ORM('databases/database.db', )
 
 
-def main():
-    start_time = datetime.datetime.now()
+def parse_oikotie():
     oikotie = Oikotie()
     oikotie.parse_by_selenium()
+
+
+def parse_eezy():
     eezy = Eezy()
     eezy.parse_by_selenium()
+
+
+def parse_barona():
     barona_parser = Barona()
     barona_parser.parse()
+
+
+def main():
+    start_time = datetime.datetime.now()
+
+    # Создание и запуск потоков
+    oikotie_thread = threading.Thread(target=parse_oikotie)
+    eezy_thread = threading.Thread(target=parse_eezy)
+    barona_thread = threading.Thread(target=parse_barona)
+
+    oikotie_thread.start()
+    eezy_thread.start()
+    barona_thread.start()
+
+    # Ожидание завершения потоков
+    oikotie_thread.join()
+    eezy_thread.join()
+    barona_thread.join()
 
     end_time = datetime.datetime.now()
 
@@ -32,3 +56,6 @@ def main():
 if __name__ == "__main__":
     db.create_tables()
     main()
+
+
+
