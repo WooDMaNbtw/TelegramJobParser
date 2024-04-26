8a740c1d9568cd326bdf92f29df53283b3d79aec
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-04-26 19:01:57 +0400

    uncommented logger

--- project/runner.py
+++ project/runner.py
@@ -21,28 +21,12 @@ bot_production = Bot(token='6795556806:AAGKyNG7T7BM1m42WGnqUZgyKvSzigugR0c')
 bot_rad = Bot(token='6795556806:AAGKyNG7T7BM1m42WGnqUZgyKvSzigugR0c')
 bot_sales = Bot(token='6795556806:AAGKyNG7T7BM1m42WGnqUZgyKvSzigugR0c')
 
-from telegram_bot import (
-    KanzuBot, DesignBot, EducationBot,
-    EngineeringBot, FinanceBot, HealthcareBot,
-    ITBot, LegalBot, ManagementBot, MarketingBot,
-    ProductionBot, ResearchAndDevelopmentBot, SalesBot)
+from telegram_bot import KanzuBot
 
 
 async def runner() -> None:
     tasks = [
         asyncio.create_task(KanzuBot.main_starter()),
-        asyncio.create_task(DesignBot.main_starter()),
-        asyncio.create_task(EducationBot.main_starter()),
-        asyncio.create_task(EngineeringBot.main_starter()),
-        asyncio.create_task(FinanceBot.main_starter()),
-        asyncio.create_task(HealthcareBot.main_starter()),
-        asyncio.create_task(ITBot.main_starter()),
-        asyncio.create_task(LegalBot.main_starter()),
-        asyncio.create_task(ManagementBot.main_starter()),
-        asyncio.create_task(MarketingBot.main_starter()),
-        asyncio.create_task(ProductionBot.main_starter()),
-        asyncio.create_task(ResearchAndDevelopmentBot.main_starter()),
-        asyncio.create_task(SalesBot.main_starter())
     ]
 
     await asyncio.gather(*tasks)
@@ -59,18 +43,18 @@ async def runner() -> None:
 
 
 if __name__ == '__main__':
-    # log_dir = os.path.join(f"{os.path.dirname(__file__)}/telegram_bot", 'TelegramLogs')
-    #
-    # if not os.path.exists(log_dir):
-    #     os.makedirs(log_dir)
-    #
-    # logging.basicConfig(level=logging.INFO)
-    # logger = logging.getLogger()
-    # file_handler = logging.FileHandler(os.path.join(log_dir, 'logs_tg.log'))
-    # formatter = logging.Formatter(
-    #     '%(asctime)s - %(levelname)s - %(filename)s - %(module)s - %(funcName)s  - %(message)s')
-    # file_handler.setFormatter(formatter)
-    # logger.addHandler(file_handler)
+    log_dir = os.path.join(f"{os.path.dirname(__file__)}/telegram_bot", 'TelegramLogs')
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
+    file_handler = logging.FileHandler(os.path.join(log_dir, 'logs_tg.log'))
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(filename)s - %(module)s - %(funcName)s  - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
     try:
         asyncio.run(runner())
